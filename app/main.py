b2ad252a2e9ba0f1df842d3e3c0d1ffc36aadf48
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-04-30 00:02:16 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,28 +1,29 @@
 class Animal:
     alive = []
-
+    
     def __init__(self, health: int, name: str, hidden: bool) -> None:
         self.health = health
         self.name = name
         self.hidden = hidden
-
+    
     def alive_animals(self) -> None:
         if self.health > 0:
-            Animal.alive.append({
-                "Name": self.name,
-                "Health": self.health,
-                "Hidden": self.hidden
-            })
+            if self.name not in Animal.alive:
+                Animal.alive.append({
+                    "Name": self.name, 
+                    "Health": self.health, 
+                    "Hidden": self.hidden
+                })
 
 
 class Herbivore(Animal):
     def hide(self) -> bool:
-        hidden_bool = self.hidden
-        return not hidden_bool
+        self.hidden is True
+        return self.hidden
 
 
 class Carnivore(Animal):
     def bite(self, herbivore: Animal) -> int:
-        if herbivore.hidden is True:
+        if herbivore.hidden is False:
             herbivore.health -= 50
         return herbivore.health
