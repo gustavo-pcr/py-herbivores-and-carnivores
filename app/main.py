f003d067618eacfdd5749d15df3793d9e045ecad
gustavo-pcr <107580274+gustavo-pcr@users.noreply.github.com>
2025-04-29 23:50:23 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+class Animal:
+    alive = []
+    
+    def __init__(self, health: int, name: str, hidden: bool) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = hidden
+    
+    def alive_animals(self) -> None:
+        if self.health > 0:
+            Animal.alive.append({
+                "Name": self.name, 
+                "Health": self.health, 
+                "Hidden": self.hidden
+            })
+
+
+class Herbivore(Animal):
+    def hide(self) -> bool:
+        hidden_bool = self.hidden
+        return not hidden_bool
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Animal) -> int:
+        herbivore.health -= 50
+        return herbivore.health
